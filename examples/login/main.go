411e10cbbd5c8e3200c98d4beb976ea36f644217
s4eedb <saeed9322@yahoo.com>
2021-12-30 17:30:08 +0330

    [example] add example file

--- examples/login/main.go
+++ examples/login/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/S4eedb/arvancloud-go"
+)
+
+func main() {
+	api, err := arvancloud.New(os.Getenv("CLOUDFLARE_API_KEY"), os.Getenv("CLOUDFLARE_API_EMAIL"))
+	if err != nil {
+		log.Print(err)
+	}
+	fmt.Println(api)
+}
